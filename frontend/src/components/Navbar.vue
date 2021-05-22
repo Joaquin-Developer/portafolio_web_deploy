776c583df1af7c340d7cb8e3990be401faf1e498
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-05-22 16:08:42 -0300

    avanzo con componente de presentacion en Vue

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -8,7 +8,7 @@
       <div class="collapse navbar-collapse" id="navbarColor03">
         <ul class="navbar-nav me-auto">
           <li class="nav-item">
-            <a class="nav-link active" href="#nav">Home
+            <a class="nav-link active" href="#nav">Inicio
               <span class="visually-hidden">(current)</span>
             </a>
           </li>
@@ -22,9 +22,31 @@
             <a class="nav-link" href="#">Curriculum</a>
           </li>
         </ul>
-        <form class="d-flex">
+        <!-- <form class="d-flex">
           <input class="form-control me-sm-2" type="text" placeholder="Search">
           <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form> -->
+        <form class="navbar-nav form-inline my-2 my-lg-0 pull-left">
+          <li class="nav-item">
+            <a class="nav-link" href="mailto:joaquin.p.olivera@gmail.com">
+              <i class="fa fa-google" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://twitter.com/JoaqunParrilla2">
+              <i class="fa fa-twitter-square" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.linkedin.com/in/joaqu%C3%ADn-parrilla-63092819b/">
+              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://t.me/Joaquin703">
+              <i class="fa fa-telegram" aria-hidden="true"></i>
+            </a>
+          </li>
         </form>
       </div>
     </div>
