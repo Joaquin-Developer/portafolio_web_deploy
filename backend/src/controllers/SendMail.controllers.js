3289497119fd342165483b4006ab7b8ca74d02ed
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-05-23 23:45:26 -0300

    actualizo backend

--- backend/src/controllers/SendMail.controllers.js
+++ backend/src/controllers/SendMail.controllers.js
@@ -1,12 +1,10 @@
+const sendMailController = {};
 
-let functions = {};
+sendMailController.send = (req, res) => {
+    const nodemailer = require("nodemailer")
+    const smtpTransport = require("nodemailer-smtp-transport")
 
-functions.send = (req, res) => {
-    const nodemailer = require("nodemailer");
-    const smtpTransport = require("nodemailer-smtp-transport");
-
-    const emailFrom = req.body.email;
-    const message = req.body.message;
+    const emailFrom = req.body.mail
     // console.log(`usuario: ${process.env.SEND_EMAIL_MAIL}, pass: ${process.env.SEND_EMAIL_PASSWORD}`);
 
     const transporter = nodemailer.createTransport(smtpTransport({
@@ -16,27 +14,27 @@ functions.send = (req, res) => {
             user: process.env.SEND_EMAIL_MAIL,
             pass: process.env.SEND_EMAIL_PASSWORD
         }
-    }));
+    }))
 
     const mailOptions = {
         from: process.env.SEND_EMAIL_MAIL,
         to: "joaquin.p.olivera@gmail.com",
         subject: "Solicitúd de Curriculum a través de portafolio web",
         // text: message
-        html: `<p>${message}</p><hr><br>Email del solicitante: ${emailFrom}`
-        // html: "<b>Hello world?</b>", // html body (example)
-    };
+        html: `<p>$El siguiente correo solicitó una copia del CV:</p><hr><strong>${emailFrom}</strong>`
+        // html: `<p>${message}</p><hr><br>Email del solicitante: ${emailFrom}`
+    }
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            console.log(error);
-            res.status(500).send("Error al enviar CV");
+            console.log(error)
+            res.status(500).send("Error al enviar CV")
         } else {
-            console.log('Email sent: ' + info.response);
-            res.status(200).send({ estado: "ok", datos: { email: emailFrom, message: message }});
+            console.log('Email sent: ' + info.response)
+            res.status(200).send({ estado: "ok", datos: { email: emailFrom, message: message }})
         }
-    });
+    })
 
 }
 
-module.exports = functions;
+module.exports = sendMailController
