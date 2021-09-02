9538ca6f6694b22089cc948d69e061d58abc5a6e
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-09-02 00:44:45 -0300

    FIX error with parameter name in response

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,4 @@
 # portafolio_web_deploy
+
+Backend de mi portafolio con Express (Node.js)
+Con este backend se recibe información y se realiza el envío de correos.
--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,17 @@ const path = require("path")
 
 // settings:
 app.set("port", process.env.PORT || 5000)
-app.use(cors({ origin: "https://joaquin-parrilla.github.io/" }))
+// app.use(cors({ origin: "https://joaquin-parrilla.github.io/" }))
+
+// cors middleware:
+app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Credentials", "true");
+    res.set("Access-Control-Allow-Origin", "https://joaquin-parrilla.github.io");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,POST,DELETE");
+    next();
+});
+
 app.use(cors())
 app.use(express.json()) // read json
 app.use(express.urlencoded({ extended: false }))    // read html forms
